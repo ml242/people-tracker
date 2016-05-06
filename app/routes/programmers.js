107044a5a6e907adddf6c1c90c98b98dfae0fa63
matt lucas <mattlucas@gmail.com>
2016-05-06 14:34:59 -0400

    build compiled

---
+++ app/routes/programmers.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model(){
+		return ['Marky Z', 'Billy G Money', 'Yehuda Robotz'];
+	}
+});
--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model() {
+    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+  }
+
 });
